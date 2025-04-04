043f947e84356f842ef7a195bc5fdf225a48d40e
Rahul Pandita <rpandita1999@outlook.com>
2025-04-04 15:17:10 +0530

    method readFile parses asm file line by line and adds converted binary code to a list

--- 6-Assembler/src/Parser.java
+++ 6-Assembler/src/Parser.java
@@ -1,4 +1,6 @@
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -8,32 +10,37 @@ import java.util.Scanner;
  */
 public class Parser {
     private final String FILE_NAME;
+    private List<String> binaryCode;
 
     public Parser(String asmFile) {
         this.FILE_NAME = asmFile;
+        this.binaryCode = new ArrayList<>();
     }
 
     // the asm file contains lines of strings
     // read file
     public void readFile() {
         try (Scanner reader = new Scanner(Paths.get(FILE_NAME))) {
-            int line_number = -1;
+//            int lineNumber = -1;
             while (reader.hasNextLine()) {
                 String line = reader.nextLine();
-                if (!(line.isBlank())) {
-                    if (!(line.startsWith("//"))) {
-                        removeInlineComments(line);
-
-                    }
+                if (line.isBlank() || line.startsWith("//")) {  // check whether whitespace or comment
+                    continue;
                 }
-
+                removeInlineComments(line);
+                Code code = new Code(line);
+                binaryCode.add(code.convertToBinary());
             }
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
+
     }
 
     private static void removeInlineComments(String line) {
-
+        int inlineCommentStartsAt = line.indexOf("//"); // will return -1 if no inline comment
+        if (inlineCommentStartsAt != -1) {
+            line = line.substring(0,inlineCommentStartsAt).trim();
+        }
     }
 }
