f80b2f071aa4bca6a2a51d6fb24380b697e1a092
Rahul Pandita <rpandita1999@outlook.com>
2025-04-04 12:37:52 +0530

    table to hold maping from c-instruction symbolic syntax to bits

--- 6-Assembler/src/Code.java
+++ 6-Assembler/src/Code.java
@@ -3,6 +3,8 @@ import java.util.Map;
 
 public class Code {
     private final Map<String,String> c_instruction_comp;
+    private final Map<String,String> c_instruction_dest;
+    private final Map<String,String> c_instruction_jump;
 
     public Code() {
         this.c_instruction_comp = new HashMap<>();
@@ -34,5 +36,26 @@ public class Code {
         c_instruction_comp.put("M-D","1000111");
         c_instruction_comp.put("D&M","1000000");
         c_instruction_comp.put("D|M","1010101");
+
+        // separate table for dest bits
+        this.c_instruction_dest = new HashMap<>();
+        c_instruction_dest.put("null","000");
+        c_instruction_dest.put("M","001");
+        c_instruction_dest.put("D","010");
+        c_instruction_dest.put("MD","011");
+        c_instruction_dest.put("A","100");
+        c_instruction_dest.put("AM","101");
+        c_instruction_dest.put("AD","110");
+        c_instruction_dest.put("AMD","111");
+
+        this.c_instruction_jump = new HashMap<>();
+        c_instruction_jump.put("null","000");
+        c_instruction_jump.put("JGT","001");
+        c_instruction_jump.put("JEQ","010");
+        c_instruction_jump.put("JGE","011");
+        c_instruction_jump.put("JLT","100");
+        c_instruction_jump.put("JNE","101");
+        c_instruction_jump.put("JLE","110");
+        c_instruction_jump.put("JMP","111");
     }
 }
