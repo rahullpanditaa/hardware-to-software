bca98162fab251810e9cbb416dbaf0a76385c23f
Rahul Pandita <rpandita1999@outlook.com>
2025-04-16 08:20:20 +0530

    Assembler V1.0 works. Generated binary code for a symbol less assembly code file

--- 6A-Assembler/src/Main.java
+++ 6A-Assembler/src/Main.java
@@ -1,21 +1,35 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) {
-        Parser p = new Parser("MaxL.asm");
-        System.out.println(p.getCurrentInstruction());
-        p.advance();
-        System.out.println(p.getCurrentInstruction());
-        System.out.println(p.instructionType());
-//        System.out.println(p.dest());
-        p.advance();
-        System.out.println(p.getCurrentInstruction());
-        System.out.println(p.instructionType());
-        System.out.println(p.dest());
-        System.out.println(p.comp());
-        System.out.println(p.jump());
+        Parser p = new Parser(args[0]);
+        Code c = new Code();
+        String hackFileName = args[0].replace(".asm",".hack");
+        start(hackFileName,p,c);
+    }
 
-
-
+    private static void start(String hackFile,Parser parser, Code codeWriter) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(hackFile))) {
+            while (parser.hasMoreLines()) {
+                parser.advance(); // get the current instruction
+                if (parser.instructionType().equals("A_INSTRUCTION")) {
+                    int decimal = Integer.valueOf(parser.getCurrentInstruction().substring(1));
+                    String bin = Integer.toBinaryString(decimal);
+                    writer.write("0" + String.format("%15s",bin).replace(' ','0'));
+                    writer.write("\n");
+                }
+                if (parser.instructionType().equals("C_INSTRUCTION")) {
+                    String cToBinary = "111" + codeWriter.dest(parser.dest()) + codeWriter.comp(parser.comp()) + codeWriter.jump(parser.jump());
+                    writer.write(cToBinary);
+                    writer.write("\n");
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 }
