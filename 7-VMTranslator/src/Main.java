61f7f9c377e46388f0d33b10baf733004575869e
Rahul Pandita <rpandita1999@outlook.com>
2025-04-17 11:40:03 +0530

    Parser currently working as expected; all tests pass

--- 7-VMTranslator/src/Main.java
+++ 7-VMTranslator/src/Main.java
@@ -2,9 +2,25 @@
 public class Main {
     public static void main(String[] args) {
         Parser p = new Parser("SimpleAdd.vm");
+        p.advance();
         System.out.println(p.getCurrentCommand());
-        for (String line : p.getVmCode()) {
-            System.out.println(line);
-        }
+        System.out.println(p.hasMoreLines());
+        System.out.println(p.commandType());
+        System.out.println(p.arg1());
+        System.out.println(p.arg2());
+        //---
+        p.advance();
+        System.out.println(p.getCurrentCommand());
+        System.out.println(p.hasMoreLines());
+        System.out.println(p.commandType());
+        System.out.println(p.arg1());
+        System.out.println(p.arg2());
+        System.out.println();
+        p.advance();
+        System.out.println(p.getCurrentCommand());
+        System.out.println(p.hasMoreLines());
+        System.out.println(p.commandType());
+        System.out.println(p.arg1());
+        System.out.println(p.arg2());
     }
 }
