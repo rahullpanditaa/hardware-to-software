ceec4620afb35e1e8244cea43aa5c9c83e8953ad
Rahul Pandita <rpandita1999@outlook.com>
2025-04-24 12:21:28 +0530

    reading source code into a string; regex to remove api block comments and multi-line comments

--- 8-Compiler/src/Tokenizer.java
+++ 8-Compiler/src/Tokenizer.java
@@ -1,73 +1,29 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
 public class Tokenizer {
-    private List<String> sourceCode;
+    private String sourceCode;
     String currentToken;
     private static List<String> keywords;
     private static List<String> symbols;
 
     public Tokenizer(String jackFile) {
-        this.sourceCode = new ArrayList<>();
-        try (BufferedReader reader = new BufferedReader(new FileReader(jackFile))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                sourceCode.add(line);
-            }
+        try {
+            // entire source code as a single string
+            this.sourceCode = new String(Files.readAllBytes(Paths.get(jackFile)));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        keywords = new ArrayList<>();
-        keywords.add("class");
-        keywords.add("constructor");
-        keywords.add("function");
-        keywords.add("method");
-        keywords.add("field");
-        keywords.add("static");
-        keywords.add("var");
-        keywords.add("int");
-        keywords.add("char");
-        keywords.add("boolean");
-        keywords.add("void");
-        keywords.add("true");
-        keywords.add("false");
-        keywords.add("null");
-        keywords.add("this");
-        keywords.add("let");
-        keywords.add("do");
-        keywords.add("if");
-        keywords.add("else");
-        keywords.add("while");
-        keywords.add("return");
-
-        symbols = new ArrayList<>();
-        symbols.add("{");
-        symbols.add("}");
-        symbols.add("(");
-        symbols.add(")");
-        symbols.add("[");
-        symbols.add("]");
-        symbols.add(".");
-        symbols.add(",");
-        symbols.add(";");
-        symbols.add("+");
-        symbols.add("-");
-        symbols.add("*");
-        symbols.add("/");
-        symbols.add("&");
-        symbols.add("|");
-        symbols.add("<");
-        symbols.add(">");
-        symbols.add("=");
-        symbols.add("~");
     }
 
     public boolean hasMoreToken() {
-        return !sourceCode.isEmpty();
+        return true;
     }
 
     // initially, there is no current token
@@ -76,16 +32,8 @@ public class Tokenizer {
     }
 
     private void removeCommentsAndWhitespace() {
-        // first, remove line comments and whitespace
-        Iterator<String> iterator = sourceCode.iterator();
-        while (iterator.hasNext()) {
-            String line = iterator.next();
-            if (line.startsWith("//") || line.isBlank()) {
-                iterator.remove();
-            }
-        }
-        // api block comments and multi line comments left
-
+        // remove all api block comments and multi-line comments
+        sourceCode = sourceCode.replaceAll("/\\*\\*?\\s\\w+\\s+\\*?.+\\s+\\*?.+\\s+\\*?/","");
 
     }
 }
