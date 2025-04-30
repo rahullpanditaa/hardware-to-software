6a9792a6d524489d1901eaf507d05de33051d318
Rahul Pandita <rpandita1999@outlook.com>
2025-04-30 11:14:29 +0530

    regex to replace parts of a string

--- 8-Compiler/src/Main.java
+++ 8-Compiler/src/Main.java
@@ -4,15 +4,25 @@ import java.util.regex.Pattern;
 public class Main {
     public static void main(String[] args) {
 
+        String cardNumber = "9876-5432-1234";
+        // XXXX-XXXX-1234
+
+        Pattern pattern = Pattern.compile("\\d{4}-\\d{4}-");
+        Matcher matcher = pattern.matcher(cardNumber);
+
+        String privateCardNumber = matcher.replaceAll("XXXX-XXXX-");
+        System.out.println(privateCardNumber);
+
 //        Pattern pattern = Pattern.compile("^agent \\d{3,}$"); // the pattern to check for and match against i.e. the regex
 //        Matcher matcher = pattern.matcher("agent 007"); // the input string (eg jack source code)
 
-        Pattern methodNamePattern = Pattern.compile("(\\w+)\\(\\)");
-        Matcher matcher = methodNamePattern.matcher("methodName()");
+//        Pattern methodNamePattern = Pattern.compile("(\\w+)\\(\\)");
+//        Matcher matcher = methodNamePattern.matcher("methodName()");
 
-        if (matcher.find()) {
-            System.out.println(matcher.group(1));
-        }
+//        if (matcher.find()) {
+//            System.out.println(matcher.group(1));
+//        }
+
 
 
     }
