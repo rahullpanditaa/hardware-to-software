15ccce40c22068c64ecb676305f3167ac80a4984
Rahul Pandita <rpandita1999@outlook.com>
2025-04-30 11:06:05 +0530

    regex pattern to get the methodName in methodName()

--- 8-Compiler/src/Main.java
+++ 8-Compiler/src/Main.java
@@ -4,9 +4,16 @@ import java.util.regex.Pattern;
 public class Main {
     public static void main(String[] args) {
 
-        Pattern pattern = Pattern.compile("\\w"); // the pattern to check for and match against i.e. the regex
-        Matcher matcher = pattern.matcher("hello"); // the input string (eg jack source code)
+//        Pattern pattern = Pattern.compile("^agent \\d{3,}$"); // the pattern to check for and match against i.e. the regex
+//        Matcher matcher = pattern.matcher("agent 007"); // the input string (eg jack source code)
 
-        System.out.println(matcher.find());
+        Pattern methodNamePattern = Pattern.compile("(\\w+)\\(\\)");
+        Matcher matcher = methodNamePattern.matcher("methodName()");
+
+        if (matcher.find()) {
+            System.out.println(matcher.group(1));
+        }
+
+
     }
 }
--- 8-Compiler/src/Tokenizer.java
+++ 8-Compiler/src/Tokenizer.java
@@ -1,2 +1,8 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class Tokenizer {
+    private String sourceCode;
+
+
 }
