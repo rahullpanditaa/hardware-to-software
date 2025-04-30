91d73e357a8531569806c8b18f1900a23e592fc7
Rahul Pandita <rpandita1999@outlook.com>
2025-04-30 11:28:45 +0530

    read source code file into a string

--- 8-Compiler/src/Main.java
+++ 8-Compiler/src/Main.java
@@ -4,16 +4,19 @@ import java.util.regex.Pattern;
 public class Main {
     public static void main(String[] args) {
 
-        Pattern pattern = Pattern.compile("^(\\d+) divided by (\\d+)$");
-        Matcher matcher = pattern.matcher("100 divided by 25");
+        Tokenizer tokenizer = new Tokenizer("Main.jack");
+        System.out.println(tokenizer.getSourceCode());
+
+//        Pattern pattern = Pattern.compile("^(\\d+) divided by (\\d+)$");
+//        Matcher matcher = pattern.matcher("100 divided by 25");
 
         // 100 divided by 2 --> 100 / 2
-        if (matcher.find()) {
-            String simplified = matcher.replaceFirst("$1/$2");  // extracting multiple groups -> $groupNumber
-            System.out.println(simplified);
-            int result = Integer.valueOf(matcher.group(1)) / Integer.valueOf(matcher.group(2));
-            System.out.println(result);
-        }
+//        if (matcher.find()) {
+//            String simplified = matcher.replaceFirst("$1/$2");  // extracting multiple groups -> $groupNumber
+//            System.out.println(simplified);
+//            int result = Integer.valueOf(matcher.group(1)) / Integer.valueOf(matcher.group(2));
+//            System.out.println(result);
+//        }
 
 //        String cardNumber = "9876-5432-1234";
 //         XXXX-XXXX-1234
--- 8-Compiler/src/Tokenizer.java
+++ 8-Compiler/src/Tokenizer.java
@@ -1,8 +1,23 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class Tokenizer {
     private String sourceCode;
+
+    public Tokenizer(String jackFile) {
+        try {
+            this.sourceCode = new String(Files.readAllBytes(Paths.get(jackFile)));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String getSourceCode() {
+        return this.sourceCode;
+    }
 
 
 }
